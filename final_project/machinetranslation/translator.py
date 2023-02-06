4379c675a6bf57c8cbc4034d9dd78fac6bde3c85
Gonzalo Raymond <gonchi_msn@hotmail.com>
2023-02-06 00:07:07 -0500

    Adding the translate en-fr and fr-en features

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 import json
-import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,8 +16,46 @@ language_translator = LanguageTranslatorV3(version = version, authenticator = au
 
 language_translator.set_service_url(url)
 
-translation = language_translator.translate(text = 'Hello, how are you today?', model_id = 'en-es').get_result()
+# Function that takes a text in English
+#  and returns its translation into French.
+def englishToFrench(englishText):
+    """
+    This function takes an English string as an argument 
+    and translates it into French using the IBM Watson AI 
+    translation service. 
+    """
+    
+    englishText = englishText.lower()
+    
+    translation = language_translator.translate(text = englishText, 
+    model_id = "en-fr").get_result()
 
+    frenchText = translation["translations"][0]["translation"]
 
+    return frenchText
 
-print(json.dumps(translation, indent = 2, ensure_ascii = False))
+# Function that takes a text in French
+#  and returns its translation into English.
+def frenchToEnglish(frenchText):
+    """
+    This function takes an French string as an argument 
+    and translates it into English using the IBM Watson AI 
+    translation service.
+    """
+   
+    frenchText = frenchText.lower()
+
+    translation = language_translator.translate(text = frenchText, 
+    model_id = "fr-en").get_result()
+
+    englishText = translation["translations"][0]["translation"]
+
+    return englishText
+
+en_message = ""
+
+fr_message = ""
+
+print(englishToFrench(en_message))
+
+print(frenchToEnglish(fr_message))
